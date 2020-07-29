2d84c3f8c3b436182062a7cf9210ecc304a8b586
Joy Johnson <joyjohnson522@gmail.com>
2020-07-29 17:54:52 -0500

    added first question

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-var quizquestion = 1
-console.log(quizquestion)
-console.log("Hello There")
-quizquestion = 2
-console.log(quizquestion)
-console.log(document)
+var quizquestion = 0
+var start = document.getElementById("startQuiz")
+var quizcont = document.getElementById("quizContainer")
+var quizresults = document.getElementById("results")
+var quest = document.getElementById("question")
+quizcont.style.display = "none"
+quizresults.style.display = "none"
+start.addEventListener("click", startquiz)
+
+var question = [
+    {
+        "question": "css is for cascading style sheets",
+        "answer": "true"
+    },
+    {
+        "question": "css is for cascading style sheets",
+        "answer": "true"
+    },
+]
+function startquiz() {
+    start.style.display = "none"
+    quizcont.style.display = "block"
+    quest.innerHTML = question[quizquestion].question
+}
