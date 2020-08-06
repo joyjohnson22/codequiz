f134fa0be4ad145c2fbd2bc8d326d9ee0c515786
Joy Johnson <joyjohnson522@gmail.com>
2020-08-05 21:56:12 -0500

    adding onclick for buttons

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ var start = document.getElementById("startQuiz")
 var quizcont = document.getElementById("quizContainer")
 var quizresults = document.getElementById("results")
 var quest = document.getElementById("question")
+var tru = document.getElementById("true")
+var fal = document.getElementById("false")
 quizcont.style.display = "none"
 quizresults.style.display = "none"
 start.addEventListener("click", startquiz)
+tru.addEventListener("click", checkanswer)
+fal.addEventListener("click", checkanswer)
 
 var question = [
     {
@@ -13,12 +17,40 @@ var question = [
         "answer": "true"
     },
     {
+        "question": "HTML stands for HyperTextMarkupLanguage",
+        "answer": "true"
+    },
+    {
         "question": "css is for cascading style sheets",
         "answer": "true"
     },
+    {
+        "question": "css is for cascading style sheets",
+        "answer": "true"
+    },
+    {
+        "question": "css is for cascading style sheets",
+        "answer": "true"
+    },
+    {
+        "question": "css is for cascading style sheets",
+        "answer": "true"
+    },
+    {
+        "question": "css is for cascading style sheets",
+        "answer": "true"
+    },
+    {
+        "question": "css is for cascading style sheets",
+        "answer": "true"
+    }
 ]
 function startquiz() {
     start.style.display = "none"
     quizcont.style.display = "block"
     quest.innerHTML = question[quizquestion].question
+}
+function checkanswer() {
+    console.log("true")
+    var useranswer = this
 }
